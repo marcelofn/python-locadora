1a6b33d1be3f3870c68b883c7872237c002e6c5e
Marcelo França <marcelo.frneves@gmail.com>
2019-03-16 09:05:39 -0300

    Implementando a playlist

--- Teste.py
+++ Teste.py
@@ -1,22 +1,28 @@
 from Filme import Filme
+from Playlist import Playlist
 from Serie import Serie
 
 
-class Teste(Serie, Filme):
+class Teste(Serie, Filme, Playlist):
+    vingadores = Filme('vingadores - guerra infinita', 2018, 160)
+    atlanta = Serie('atlanta', 2018, 2)
+    tmep = Filme('todo mundo em panico', 1999, 100)
+    demolidor = Serie('demolidor', 2016, 2)
 
-    vingadores = Filme('vingadores', 2018, 150)
-    atlanta = Serie('atlanta', 2019, 2)
     vingadores.dar_likes()
     vingadores.dar_likes()
+    vingadores.dar_likes()
     atlanta.dar_likes()
     atlanta.dar_likes()
-    atlanta.dar_likes()
+    tmep.dar_likes()
+    tmep.dar_likes()
+    demolidor.dar_likes()
+    demolidor.dar_likes()
 
-    lista_programas = [vingadores, atlanta]
+    listinha = [atlanta, vingadores, demolidor, tmep]
+    minha_playlist = Playlist('fim de semana', listinha)
 
-    for programas in lista_programas:
-        print(programas)
+    for programa in minha_playlist.listagem:
+        print(programa)
 
-
-
-
+    print(f'Tamanho: {len(minha_playlist.listagem)}')
